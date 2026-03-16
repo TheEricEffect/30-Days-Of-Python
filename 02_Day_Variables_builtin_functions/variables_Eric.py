e789aaec26f3a0479a5635973b2bedcdb9ec5aca
TheEricEffect <ericyhh2011@gmail.com>
2026-03-16 02:38:04 +0000

    Update variables_Eric.py

--- 02_Day_Variables_builtin_functions/variables_Eric.py
+++ 02_Day_Variables_builtin_functions/variables_Eric.py
@@ -1,1 +1,30 @@
-
+first_name = "Eric"
+last_name = "Huang"
+full_name = "Eric Huang"
+country = "Ireland"
+city = "Longford"
+age = 14
+year = 2011
+is_married = false
+first_name, age, city, country = "Eric", 14, "Longford", "Ireland"
+len(first_name)
+print(len(first_name, last_name))
+num_one, num_two = 5, 4
+total = num_one + num_two
+diff = num_two - num_one
+product = num_two * num_one
+division = num_one / num_two
+remainder = num_two % num_one
+exp = num_one ** num_two
+floor_division = num_one // num_two
+radius = 30
+area_of_circle = radius ** 2 * 3.1415926535897
+circum_of_circle = radius * 2 * 3.1415926535897
+radius = input(enter radius: )
+print(radius ** 2 * 3.1415926535897)
+first_name = input(Enter firt name: )
+last_name = input(Enter last name: )
+country = input(Enter country: )
+age = input(Enter age: )
+age = input(Enter age: )
+help('keywords')
